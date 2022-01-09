b54013b0c0cf095f8bd1be29d33bef6f077ddf1c
IPeter1982 <37334734+IPeter1982@users.noreply.github.com>
2022-01-09 22:30:14 +0100

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -45,7 +45,11 @@ namespace WishList.Controllers
         public IActionResult Delete(int id)
         {
             var user = _userManager.GetUserAsync(HttpContext.User).Result;
-            var item = _context.Items.Where(i => i.User.Id == user.Id).FirstOrDefault(e => e.Id == id);
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            if (item.User.Id != user.Id)
+            {
+                return Unauthorized();
+            }
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
